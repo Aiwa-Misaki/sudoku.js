0d7b03f081aae1d047553c87b4bdd6c530f7c463
Aiwa <zyxwhisper@qq.com>
2022-12-10 16:56:58 +0800

    added shadow

--- pdfGenerator.js
+++ pdfGenerator.js
@@ -24,6 +24,9 @@ let docInfo = class {
         return puzzleWithLastAns;
     }
 }
+let boardStyle = class {
+
+}
 
 function pdfGenerator() {
     let sudoku = sudokuInit();
@@ -41,17 +44,19 @@ function pdfGenerator() {
     }
 
     function drawPDF() {
-        let puz = sudoku.generate("hard");
+        let puz = sudoku.generate(34);
         let ans = sudoku.solve(puz);
-        _draw_board(puz, [page_size[0] * 0.1, page_size[0] * 0.1], page_size[0] * 0.8, 1);
-        _draw_line_shadow(1, [page_size[0] * 0.1, page_size[0] * 0.1], page_size[0] * 0.8, undefined);
-        file.text("PDF Generator Test", 10, 10);
+        let ans_size = page_size[0] * 0.2;
+        let puz_size = page_size[0] * 0.8;
+        _draw_puzzle(puz, [page_size[0] * 0.1, page_size[0] * 0.2], puz_size, 1);
+        _draw_puzzle(ans, [page_size[0] * 0.7, page_size[1] * 0.8], ans_size, 1)
+        file.text("PDF Generator Test", 10, 10, page_size[0] * 0.2, 1);
         file.save("a4.pdf")
     }
 
     // generate multiple sudoku puzzles and return [[puzzles][solutions]]
 
-    function _generate_sudoku(num = 1, difficulty = "medium") {
+    function _generate_sudoku(num = 1, difficulty = "hard") {
         let puzzles_solutions = [[], []];
         for (let i = 0; i < num; i++) {
             let puzzle = sudoku.generate(difficulty);
@@ -63,9 +68,11 @@ function pdfGenerator() {
     }
 
     // draw a page using a single puzzle
-    function _draw_board(puzzleMat, left_top, board_width, height_width_ratio) {
+    function _draw_puzzle(puzzleMat, left_top, board_width, height_width_ratio) {
         let board_height = board_width * height_width_ratio;
         puzzleMat = sudoku.board_string_to_grid(puzzleMat);
+        file.setFont("Courier");
+        file.setFontSize(18);
         for (let row = 0; row < 9; row++) {
             for (let col = 0; col < 9; col++) {
                 let num = puzzleMat[row][col];
@@ -73,34 +80,34 @@ function pdfGenerator() {
                     // console.log(num);
                     let positionX = left_top[0] + row * board_width / 9;
                     let positionY = left_top[1] + col * board_height / 9;
+                    file.setFillColor(235, 235, 235);
+                    file.rect(positionX, positionY, board_width / 9, board_width / 9, 'F');
                     file.text(num, positionX + 0.5 * board_width / 9, positionY + 0.5 * board_height / 9, {
                         align: "center",
                         baseline: "middle"
                     });
-                    file.setDrawColor(100, 100, 0);
-                    file.rect(positionX, positionY, board_width / 9, board_width / 9);
                 }
             }
         }
-
+        _draw_line(1, left_top, board_width);
     }
 
     // draw a page using solution
-    function _draw_sketch(solutionMat) {
+    function _draw_sketch() {
 
     }
 
-    function _draw_solution(solutionMat) {
+    function _draw_solution(solutionMat, left_top, board_width, height_width_ratio) {
 
     }
 
     // draw chessboard with 9 cols and 9 rows
     // can be used in puzzle, sketch and solution
-    function _draw_line_shadow(height_width_ratio, left_top, width, positionArr) {
+    function _draw_line(height_width_ratio, left_top, width) {
         let boardWidth = width;
         let boardHeight = boardWidth * height_width_ratio;
-        file.setLineJoin("square");
-        // _fill_cube(positionArr);
+        file.setLineJoin(2);
+        file.setLineCap(2);
         // draw the internal lines
         for (let index = 1; index < 9; index++) {
             // set the line styles
@@ -128,18 +135,11 @@ function pdfGenerator() {
             let south = [north[0], north[1] + boardHeight];
             file.line(north[0], north[1], south[0], south[1]);
         }
-
-        // [[x1,y1],[x2,y2],...]
-        function _fill_cube(positionArr) {
-            for (let cube in positionArr) {
-                let cube_left_top = [left_top[0] + cube[0] * boardWidth / 9, left_top[1] + cube[1] * boardHeight / 9];
-                file.rect(cube_left_top[0], cube_left_top[1], boardWidth / 9, boardHeight / 9);
-            }
-        }
     }
 
     initialize();
     drawPDF();
+    console.log(file.getFontList())
 }
 
 
