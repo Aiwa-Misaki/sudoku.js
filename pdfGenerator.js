97e87ae1864490454715c29240791477ed8e00c5
Aiwa <zyxwhisper@qq.com>
2022-12-10 22:42:00 +0800

    reformatted, added sketch area

--- pdfGenerator.js
+++ pdfGenerator.js
@@ -2,31 +2,35 @@ import {jsPDF} from "jspdf";
 import {sudokuInit} from "./sudoku.js";
 
 
-let docInfo = class {
-    constructor(sudokuMats, solutionMats, pageSize) {
-        this.sudokuMats = sudokuMats;
-        this.solutionMats = solutionMats;
-        this.puzzleNum = sudokuMats.length;
-    }
-
-    getPage(pageNum) {
-        let puzzleWithLastAns = [undefined, undefined];
-        if (pageNum < 0 || pageNum > this.puzzleNum) {
-            console.log("err");
-        } else {
-            if (pageNum < this.puzzleNum) {
-                puzzleWithLastAns[0] = this.sudokuMats[pageNum];
-            }
-            if (pageNum > 0) {
-                puzzleWithLastAns[1] = this.solutionMats[pageNum - 1];
-            }
-        }
-        return puzzleWithLastAns;
+let lineStyle = class {
+    constructor(width, color) {
+        this.width = width;
+        this.color = color;
     }
 }
 let boardStyle = class {
-
+    constructor(borderStyle, innerStyle, normalStyle, shadowColor, fontSize) {
+        this.borderStyle = borderStyle;
+        this.innerStyle = innerStyle;
+        this.normalStyle = normalStyle;
+        this.shadowColor = shadowColor;
+        this.fontSize = fontSize;
+    }
 }
+let sizePos = class {
+    constructor(leftTop, width, height_width_ratio) {
+        this.leftTop = leftTop;
+        this.width = width;
+        this.height_width_ratio = height_width_ratio;
+    }
+}
+
+const BOARD_STYLE = {
+    PUZZLE_STYLE: new boardStyle(new lineStyle(0.9, [100, 100, 100]), new lineStyle(0.9, [200, 200, 200]), new lineStyle(0.3, [200, 200, 200]), [240, 240, 240], 22),
+    SKETCH_STYLE: new boardStyle(new lineStyle(0.8, [220, 220, 220]), new lineStyle(0.8, [220, 220, 220]), new lineStyle(0.3, [220, 220, 220]), [240, 240, 240], 0),
+    ANSWER_STYLE: new boardStyle(new lineStyle(0.4, [100, 100, 100]), new lineStyle(0.4, [100, 100, 100]), new lineStyle(0.2, [100, 100, 100]), [255, 255, 255], 9)
+}
+
 
 function pdfGenerator() {
     let sudoku = sudokuInit();
@@ -34,11 +38,18 @@ function pdfGenerator() {
     let doc_info = undefined;
     let page_size = undefined;
 
+    let BOARD_SIZE_POS = undefined;
+
     function initialize(psize = [168, 238], num = 1, difficulty = "medium") {
         page_size = psize;
         file = new jsPDF({
             format: psize
         });
+        BOARD_SIZE_POS = {
+            PUZZLE: new sizePos([psize[0] * 0.125, psize[1] * 0.12], psize[0] * 0.75, 1),
+            SKETCH: new sizePos([psize[0] * 0.125, psize[1] * 0.18 + psize[0] * 0.75], psize[0] * 0.75, 0.5),
+            ANSWER: new sizePos([psize[0] * 0.05, psize[0] * 0.05], psize[0] * 0.16, 1)
+        }
         let puzzle_info = _generate_sudoku(num, difficulty);
         doc_info = new docInfo(puzzle_info[0], puzzle_info[1]);
     }
@@ -46,11 +57,18 @@ function pdfGenerator() {
     function drawPDF() {
         let puz = sudoku.generate(34);
         let ans = sudoku.solve(puz);
-        let ans_size = page_size[0] * 0.2;
-        let puz_size = page_size[0] * 0.8;
-        _draw_puzzle(puz, [page_size[0] * 0.1, page_size[0] * 0.2], puz_size, 1);
-        _draw_puzzle(ans, [page_size[0] * 0.7, page_size[1] * 0.8], ans_size, 1)
-        file.text("PDF Generator Test", 10, 10, page_size[0] * 0.2, 1);
+        _draw_puzzle(puz, BOARD_STYLE.PUZZLE_STYLE, BOARD_SIZE_POS.PUZZLE);
+        // _draw_puzzle(ans, BOARD_STYLE.ANSWER_STYLE, BOARD_SIZE_POS.ANSWER);
+        _draw_puzzle(undefined, BOARD_STYLE.SKETCH_STYLE, BOARD_SIZE_POS.SKETCH)
+        file.setTextColor("black");
+        file.setFontSize(20);
+        file.text("Daily Sudoku ___ ___ ___", page_size[0] * 0.05, page_size[0] * 0.1);
+        file.setTextColor(150, 150, 150);
+        file.setFontSize(15);
+        file.text("SKETCH AREA", page_size[0] * 0.15, page_size[1] * 0.15 + page_size[0] * 0.75, {
+            align: "center",
+            baseline: "middle"
+        });
         file.save("a4.pdf")
     }
 
@@ -68,28 +86,35 @@ function pdfGenerator() {
     }
 
     // draw a page using a single puzzle
-    function _draw_puzzle(puzzleMat, left_top, board_width, height_width_ratio) {
+    function _draw_puzzle(puzzleMat, style, sizePos) {
+        let left_top = sizePos.leftTop;
+        let board_width = sizePos.width;
+        let height_width_ratio = sizePos.height_width_ratio;
         let board_height = board_width * height_width_ratio;
         puzzleMat = sudoku.board_string_to_grid(puzzleMat);
         file.setFont("Courier");
-        file.setFontSize(18);
-        for (let row = 0; row < 9; row++) {
-            for (let col = 0; col < 9; col++) {
-                let num = puzzleMat[row][col];
-                if (num !== ".") {
-                    // console.log(num);
-                    let positionX = left_top[0] + row * board_width / 9;
-                    let positionY = left_top[1] + col * board_height / 9;
-                    file.setFillColor(235, 235, 235);
-                    file.rect(positionX, positionY, board_width / 9, board_width / 9, 'F');
-                    file.text(num, positionX + 0.5 * board_width / 9, positionY + 0.5 * board_height / 9, {
-                        align: "center",
-                        baseline: "middle"
-                    });
+        file.setFontSize(style.fontSize);
+        // draw the text
+        if (puzzleMat.length !== 0)
+            for (let row = 0; row < 9; row++) {
+                for (let col = 0; col < 9; col++) {
+                    let num = puzzleMat[row][col];
+                    if (num !== ".") {
+                        // console.log(num);
+                        let positionX = left_top[0] + row * board_width / 9;
+                        let positionY = left_top[1] + col * board_height / 9;
+                        // console.log(positionX, positionY)
+                        file.setFillColor(style.shadowColor[0], style.shadowColor[1], style.shadowColor[2]);
+                        // console.log(left_top, board_width, board_height)
+                        file.rect(positionX, positionY, board_width / 9, board_width / 9, 'F');
+                        file.text(num, positionX + 0.5 * board_width / 9, positionY + 0.5 * board_height / 9, {
+                            align: "center",
+                            baseline: "middle"
+                        });
+                    }
                 }
             }
-        }
-        _draw_line(1, left_top, board_width);
+        _draw_line(style, sizePos);
     }
 
     // draw a page using solution
@@ -103,27 +128,28 @@ function pdfGenerator() {
 
     // draw chessboard with 9 cols and 9 rows
     // can be used in puzzle, sketch and solution
-    function _draw_line(height_width_ratio, left_top, width) {
-        let boardWidth = width;
-        let boardHeight = boardWidth * height_width_ratio;
+    function _draw_line(style, sizePos) {
+        let boardWidth = sizePos.width;
+        let boardHeight = boardWidth * sizePos.height_width_ratio;
+        let left_top = sizePos.leftTop;
         file.setLineJoin(2);
         file.setLineCap(2);
         // draw the internal lines
         for (let index = 1; index < 9; index++) {
             // set the line styles
             if (index === 3 || index === 6) { // the inner division line
-                file.setDrawColor(200, 200, 200)
-                file.setLineWidth(0.9);
+                file.setDrawColor(style.innerStyle.color[0], style.innerStyle.color[1], style.innerStyle.color[2])
+                file.setLineWidth(style.innerStyle.width);
             } else { // normal lines
-                file.setDrawColor(200, 200, 200);
-                file.setLineWidth(0.3);
+                file.setDrawColor(style.normalStyle.color[0], style.normalStyle.color[1], style.normalStyle.color[2]);
+                file.setLineWidth(style.normalStyle.width);
             }
             _draw_crossline(index);
         }
-        // draw the boundary lines
+        // draw the borderlines
         for (let index = 0; index <= 9; index += 9) {
-            file.setDrawColor(100, 100, 100);
-            file.setLineWidth(0.9);
+            file.setDrawColor(style.borderStyle.color[0], style.borderStyle.color[1], style.borderStyle.color[2]);
+            file.setLineWidth(style.borderStyle.width);
             _draw_crossline(index);
         }
 
@@ -139,7 +165,6 @@ function pdfGenerator() {
 
     initialize();
     drawPDF();
-    console.log(file.getFontList())
 }
 
 
